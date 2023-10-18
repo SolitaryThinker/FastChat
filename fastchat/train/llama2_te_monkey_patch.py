79f7d8b6b5c9d50c1e58d61eb851ce52a3231038
William Lin <wlsaidhi@gmail.com>
2023-10-18 14:50:33 -0700

    te monkey patch

--- fastchat/train/llama2_te_monkey_patch.py
+++ fastchat/train/llama2_te_monkey_patch.py
@@ -10,19 +10,91 @@ from flash_attn.flash_attn_interface import (
 )
 from transformers.models.llama.modeling_llama import (
     LlamaAttention,
+    LlamaFlashAttention2,
     LlamaModel,
     rotate_half,
 )
 
+
+# monkey patch for LlamaDecoderLayer
 def __init__(self, config: LlamaConfig):
-    super().__init__(config)
-    self.padding_idx = config.pad_token_id
-    self.vocab_size = config.vocab_size
+    super().__init__()
+    self.hidden_size = config.hidden_size
+    self.self_attn = (
+        LlamaAttention(config=config)
+        if not getattr(config, "_flash_attn_2_enabled", False)
+        else LlamaFlashAttention2(config=config)
+    )
+    # self.mlp = LlamaMLP(config)
+    self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+    # self.post_attention_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+    self.layernorm_mlp = te.LayerNormMLP(
+            config.hidden_size,
+            eps=config.rms_norm_eps,
+            bias=False,
+            normalization='RMSNorm',
+            activation='swiglu')
 
-    self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
-    self.layers = nn.ModuleList([LlamaDecoderLayer(config) for _ in range(config.num_hidden_layers)])
-    self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+# monkey patch for LlamaDecoderLayer
+def forward(
+    self,
+    hidden_states: torch.Tensor,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_value: Optional[Tuple[torch.Tensor]] = None,
+    output_attentions: Optional[bool] = False,
+    use_cache: Optional[bool] = False,
+    padding_mask: Optional[torch.LongTensor] = None,
+) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+    """
+    Args:
+        hidden_states (`torch.FloatTensor`): input to the layer of shape `(batch, seq_len, embed_dim)`
+        attention_mask (`torch.FloatTensor`, *optional*): attention mask of size
+            `(batch, 1, tgt_len, src_len)` where padding elements are indicated by very large negative values.
+        output_attentions (`bool`, *optional*):
+            Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+            returned tensors for more detail.
+        use_cache (`bool`, *optional*):
+            If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding
+            (see `past_key_values`).
+        past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
+    """
 
-    self.gradient_checkpointing = False
-    # Initialize weights and apply final processing
-    self.post_init()
+    residual = hidden_states
+
+    hidden_states = self.input_layernorm(hidden_states)
+
+    # Self Attention
+    hidden_states, self_attn_weights, present_key_value = self.self_attn(
+        hidden_states=hidden_states,
+        attention_mask=attention_mask,
+        position_ids=position_ids,
+        past_key_value=past_key_value,
+        output_attentions=output_attentions,
+        use_cache=use_cache,
+        padding_mask=padding_mask,
+    )
+    hidden_states = residual + hidden_states
+
+    # Fully Connected
+    residual = hidden_states
+    # hidden_states = self.post_attention_layernorm(hidden_states)
+    # hidden_states = self.mlp(hidden_states)
+
+    # FIXME looking in to is_first_microbatch
+    hidden_states = self.layernorm_mlp(hidden_states)
+    hidden_states = residual + hidden_states
+
+    outputs = (hidden_states,)
+
+    if output_attentions:
+        outputs += (self_attn_weights,)
+
+    if use_cache:
+        outputs += (present_key_value,)
+
+    return outputs
+
+def replace_llama_with_te():
+    LlamaDecoderLayer.__init__ = __init__
+    LlamaDecoderLayer.forward = forward
